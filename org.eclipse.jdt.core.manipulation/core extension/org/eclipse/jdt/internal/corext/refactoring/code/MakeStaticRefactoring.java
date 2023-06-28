19618bba816aeb32b8bc7df4f6fd0d7b3c1cba8e
Michael Bangas <bami1022@h-ka.de>
2023-06-28 09:13:12 +0200

    Improved code readability for inner class ChangeInstanceUsagesInMethodBody

--- org.eclipse.jdt.core.manipulation/core extension/org/eclipse/jdt/internal/corext/refactoring/code/MakeStaticRefactoring.java
+++ org.eclipse.jdt.core.manipulation/core extension/org/eclipse/jdt/internal/corext/refactoring/code/MakeStaticRefactoring.java
@@ -402,88 +402,9 @@ public class MakeStaticRefactoring extends Refactoring {
 		public boolean visit(SimpleName node) {
 			IBinding binding= node.resolveBinding();
 			if (binding instanceof IVariableBinding) {
-				IVariableBinding variableBinding= (IVariableBinding) binding;
-
-				//Check if we are inside a anonymous class
-				ITypeBinding declaringClass= variableBinding.getDeclaringClass();
-				if (declaringClass != null) {
-					boolean isLocal= declaringClass.isLocal();
-					boolean isAnonymous= declaringClass.isAnonymous();
-					boolean isMember= declaringClass.isMember();
-					boolean isNested= declaringClass.isNested();
-					boolean isTopLevel= declaringClass.isTopLevel();
-					if (!isTopLevel || isNested || isAnonymous || isLocal || isMember) {
-						return super.visit(node);
-					}
-				}
-
-
-				if (variableBinding.isField() && !Modifier.isStatic(variableBinding.getModifiers())) {
-					ASTNode parent= node.getParent();
-
-					//this ensures only the leftmost SimpleName or QualifiedName gets changed see "testConcatenatedFieldAccessAndQualifiedNames"
-					if (parent instanceof FieldAccess) {
-						FieldAccess fieldAccess= (FieldAccess) parent;
-						if (fieldAccess.getExpression() != node) {
-							return super.visit(node);
-						}
-					} else if (parent instanceof QualifiedName) {
-						QualifiedName qualifiedName= (QualifiedName) parent;
-						if (qualifiedName.getQualifier() != node) {
-							return super.visit(node);
-						}
-					}
-
-					FieldAccess replacement= fAst.newFieldAccess();
-					replacement.setExpression(fAst.newSimpleName(fParamName));
-					replacement.setName(fAst.newSimpleName(node.getIdentifier()));
-					if (parent instanceof SuperFieldAccess) {
-						fstatus.merge(RefactoringStatus.createWarningStatus(RefactoringCoreMessages.MakeStaticRefactoring_selected_method_uses_super_field_access));
-						fRewrite.replace(parent, replacement, null);
-
-					} else {
-						fRewrite.replace(node, replacement, null);
-					}
-
-
-					fHasInstanceUsages= true;
-				}
+				modifyFieldUsage(node, binding);
 			} else if (binding instanceof IMethodBinding) {
-				IMethodBinding methodBinding= (IMethodBinding) binding;
-
-				//Check if we are inside a anonymous class
-				ITypeBinding declaringClass= methodBinding.getDeclaringClass();
-				if (declaringClass != null) {
-					boolean isLocal= declaringClass.isLocal();
-					boolean isAnonymous= declaringClass.isAnonymous();
-					boolean isMember= declaringClass.isMember();
-					boolean isNested= declaringClass.isNested();
-					boolean isTopLevel= declaringClass.isTopLevel();
-					if (!isTopLevel || isNested || isAnonymous || isLocal || isMember) {
-						return super.visit(node);
-					}
-				}
-
-
-				if (!Modifier.isStatic(methodBinding.getModifiers())) {
-					fHasInstanceUsages= true;
-
-					if (isRecursion(node)) {
-						fstatus.merge(RefactoringStatus.createFatalErrorStatus(RefactoringCoreMessages.MakeStaticRefactoring_not_available_for_recursive_methods));
-						return super.visit(node);
-					}
-
-					ASTNode parent= node.getParent();
-					SimpleName replacementExpression= fAst.newSimpleName(fParamName);
-					if (parent instanceof MethodInvocation) {
-						MethodInvocation methodInvocation= (MethodInvocation) parent;
-						Expression optionalExpression= methodInvocation.getExpression();
-
-						if (optionalExpression == null) {
-							fRewrite.set(methodInvocation, MethodInvocation.EXPRESSION_PROPERTY, replacementExpression, null);
-						}
-					}
-				}
+				modifyInstanceMethodUsage(node, binding);
 			}
 			return super.visit(node);
 		}
@@ -493,66 +414,123 @@ public class MakeStaticRefactoring extends Refactoring {
 			ASTNode parentNode= node.getParent();
 
 			Name qualifier= node.getQualifier();
+
 			if (qualifier != null) {
 				IBinding qualifierBinding= qualifier.resolveBinding();
 				ITypeBinding typeBinding= (ITypeBinding) qualifierBinding;
-				if (typeBinding != null) {
-					boolean isLocal= typeBinding.isLocal();
-					boolean isAnonymous= typeBinding.isAnonymous();
-					boolean isMember= typeBinding.isMember();
-					boolean isNested= typeBinding.isNested();
-					boolean isTopLevel= typeBinding.isTopLevel();
-					if (!isTopLevel || isNested || isAnonymous || isLocal || isMember) {
-						return super.visit(node);
-					}
+				if (isInsideAnonymousClass(typeBinding)) {
+					return super.visit(node);
+				}
+			} else {
+				if (parentIsAnonymousClass(parentNode)) {
+					return super.visit(node);
 				}
 			}
-
-			if (qualifier == null) {
-				while (parentNode != null) {
-					if (parentNode instanceof AnonymousClassDeclaration) {
-						// 'this' keyword is inside an anonymous class, skip
-						return super.visit(node);
-					}
-					parentNode= parentNode.getParent();
-				}
-			}
-
 
 			// 'this' keyword is not inside an anonymous class
-			fHasInstanceUsages= true;
-			SimpleName replacement= fAst.newSimpleName(fParamName);
-			fRewrite.replace(node, replacement, null);
+			replaceThisExpression(node);
 
 			return super.visit(node);
 		}
-
 
 		@Override
 		public boolean visit(ClassInstanceCreation node) {
 			ITypeBinding typeBinding= node.getType().resolveBinding();
 			if (typeBinding != null && typeBinding.isMember() && !Modifier.isStatic(typeBinding.getModifiers())) {
 				fHasInstanceUsages= true;
-				ClassInstanceCreation replacement= fAst.newClassInstanceCreation();
-				replacement.setType((Type) ASTNode.copySubtree(fAst, node.getType()));
-				replacement.setExpression(fAst.newSimpleName(fParamName));
-				for (Object arg : node.arguments()) {
-					replacement.arguments().add(ASTNode.copySubtree(fAst, (Expression) arg));
-				}
-				fRewrite.replace(node, replacement, null);
+				replaceClassInstanceCreation(node);
 			}
 			return super.visit(node);
 		}
 
-		@Override
-		public boolean visit(SuperMethodInvocation node) {
-			fstatus.merge(RefactoringStatus.createFatalErrorStatus(RefactoringCoreMessages.MakeStaticRefactoring_explicit_super_method_invocation));
-			return super.visit(node);
+		private void modifyFieldUsage(SimpleName node, IBinding binding) {
+			IVariableBinding variableBinding= (IVariableBinding) binding;
+
+			//Check if we are inside a anonymous class
+			ITypeBinding declaringClass= variableBinding.getDeclaringClass();
+			if (isInsideAnonymousClass(declaringClass)) {
+				return;
+			}
+
+			if (variableBinding.isField() && !Modifier.isStatic(variableBinding.getModifiers())) {
+				ASTNode parent= node.getParent();
+
+				//this ensures only the leftmost SimpleName or QualifiedName gets changed see "testConcatenatedFieldAccessAndQualifiedNames"
+				if (isConcatenatedFieldAccessOrQualifiedName(node, parent)) {
+					return;
+				}
+
+				replaceFieldAccess(node, parent);
+
+				fHasInstanceUsages= true;
+			}
 		}
 
+		private void modifyInstanceMethodUsage(SimpleName node, IBinding binding) {
+			IMethodBinding methodBinding= (IMethodBinding) binding;
+			ITypeBinding declaringClass= methodBinding.getDeclaringClass();
+			//Check if we are inside a anonymous class
+			if (isInsideAnonymousClass(declaringClass)) {
+				return;
+			}
 
+			if (!Modifier.isStatic(methodBinding.getModifiers())) {
+				fHasInstanceUsages= true;
 
-		private boolean isRecursion(SimpleName node) {
+				//check if method is recursive
+				if (isRecursive(node)) {
+					fstatus.merge(RefactoringStatus.createFatalErrorStatus(RefactoringCoreMessages.MakeStaticRefactoring_not_available_for_recursive_methods));
+					return;
+				}
+
+				replaceMethodInvocation(node);
+			}
+		}
+
+		private boolean isInsideAnonymousClass(ITypeBinding declaringClass) {
+			if (declaringClass != null) {
+				boolean isLocal= declaringClass.isLocal();
+				boolean isAnonymous= declaringClass.isAnonymous();
+				boolean isMember= declaringClass.isMember();
+				boolean isNested= declaringClass.isNested();
+				boolean isTopLevel= declaringClass.isTopLevel();
+				if (!isTopLevel || isNested || isAnonymous || isLocal || isMember) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		private boolean isConcatenatedFieldAccessOrQualifiedName(SimpleName node, ASTNode parent) {
+			if (parent instanceof FieldAccess) {
+				FieldAccess fieldAccess= (FieldAccess) parent;
+				if (fieldAccess.getExpression() != node) {
+					return true;
+				}
+			} else if (parent instanceof QualifiedName) {
+				QualifiedName qualifiedName= (QualifiedName) parent;
+				if (qualifiedName.getQualifier() != node) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		private void replaceFieldAccess(SimpleName node, ASTNode parent) {
+			FieldAccess replacement= fAst.newFieldAccess();
+			replacement.setExpression(fAst.newSimpleName(fParamName));
+			replacement.setName(fAst.newSimpleName(node.getIdentifier()));
+
+			if (parent instanceof SuperFieldAccess) {
+				//Warning is needed to inform user of possible changes in semantics when SuperFieldAccess is found
+				fstatus.merge(RefactoringStatus.createWarningStatus(RefactoringCoreMessages.MakeStaticRefactoring_selected_method_uses_super_field_access));
+				fRewrite.replace(parent, replacement, null);
+			} else {
+				fRewrite.replace(node, replacement, null);
+			}
+		}
+
+		private boolean isRecursive(SimpleName node) {
 			IMethodBinding nodeMethodBinding= (IMethodBinding) node.resolveBinding();
 			IMethodBinding outerMethodBinding= fTargetMethodDeclaration.resolveBinding();
 
@@ -560,6 +538,52 @@ public class MakeStaticRefactoring extends Refactoring {
 				return true;
 			}
 			return false;
+		}
+
+		private void replaceMethodInvocation(SimpleName node) {
+			ASTNode parent= node.getParent();
+			SimpleName replacementExpression= fAst.newSimpleName(fParamName);
+			if (parent instanceof MethodInvocation) {
+				MethodInvocation methodInvocation= (MethodInvocation) parent;
+				Expression optionalExpression= methodInvocation.getExpression();
+
+				if (optionalExpression == null) {
+					fRewrite.set(methodInvocation, MethodInvocation.EXPRESSION_PROPERTY, replacementExpression, null);
+				}
+			}
+		}
+
+		private boolean parentIsAnonymousClass(ASTNode parentNode) {
+			while (parentNode != null) {
+				if (parentNode instanceof AnonymousClassDeclaration) {
+					// 'this' keyword is inside an anonymous class, skip
+					return true;
+				}
+				parentNode= parentNode.getParent();
+			}
+			return false;
+		}
+
+		private void replaceThisExpression(ThisExpression node) {
+			fHasInstanceUsages= true;
+			SimpleName replacement= fAst.newSimpleName(fParamName);
+			fRewrite.replace(node, replacement, null);
+		}
+
+		private void replaceClassInstanceCreation(ClassInstanceCreation node) {
+			ClassInstanceCreation replacement= fAst.newClassInstanceCreation();
+			replacement.setType((Type) ASTNode.copySubtree(fAst, node.getType()));
+			replacement.setExpression(fAst.newSimpleName(fParamName));
+			for (Object arg : node.arguments()) {
+				replacement.arguments().add(ASTNode.copySubtree(fAst, (Expression) arg));
+			}
+			fRewrite.replace(node, replacement, null);
+		}
+
+		@Override
+		public boolean visit(SuperMethodInvocation node) {
+			fstatus.merge(RefactoringStatus.createFatalErrorStatus(RefactoringCoreMessages.MakeStaticRefactoring_explicit_super_method_invocation));
+			return super.visit(node);
 		}
 	}
 
@@ -623,7 +647,9 @@ public class MakeStaticRefactoring extends Refactoring {
 
 		IMethod method= org.eclipse.jdt.internal.corext.refactoring.Checks.findMethod(methodName, parameterAmount, false, type);
 
-		if (method == null) { return; }
+		if (method == null) {
+			return;
+		}
 
 		//check if parameter types match (also compare new parameter that is added by refactoring)
 		String className= ((TypeDeclaration) fTargetMethodDeclaration.getParent()).getName().toString();
@@ -640,7 +666,9 @@ public class MakeStaticRefactoring extends Refactoring {
 
 		for (int i= 0; i < paramTypesOfFoundMethod.length; i++) {
 			contains= paramTypesOfSelectedMethodExtended[i].equals(paramTypesOfFoundMethod[i]);
-			if (!contains) { return; }
+			if (!contains) {
+				return;
+			}
 		}
 
 		status.merge(RefactoringStatus
